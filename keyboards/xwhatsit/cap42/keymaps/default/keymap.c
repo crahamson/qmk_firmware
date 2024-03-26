aa9f7685c2b2e41e7bec5bf7ba93cc42353e9155
durken <git@durken.se>
2024-03-26 23:09:41 +0100

    some updates to cap42

--- keyboards/xwhatsit/cap42/keymaps/default/keymap.c
+++ keyboards/xwhatsit/cap42/keymaps/default/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "keymap_swedish.h"
+#include "sendstring_swedish.h"
 
 #define NUM_SPC LT(NUM, KC_SPC)
 #define FN_ENT LT(FN, KC_ENT)
@@ -8,37 +9,43 @@
 #define ALT_DEL MT(MOD_LALT, KC_DEL)
 #define SFT_INS LSFT(KC_INS)
 
+enum custom_keycodes {
+    rarrow = SAFE_RANGE,
+    larrow,
+    others0,
+};
+
 // Combos
-enum combos {
-  ST_CTL,
-  RS_SFT,
-  AR_GUI,
-  RT_ALT,
-  NE_CTL,
-  EI_SFT,
-  IO_GUI,
-  NI_ALT
-};
-
-const uint16_t PROGMEM st_combo[] = {KC_S, KC_T, COMBO_END};
-const uint16_t PROGMEM rs_combo[] = {KC_R, KC_S, COMBO_END};
-const uint16_t PROGMEM ar_combo[] = {KC_A, KC_R, COMBO_END};
-const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
-const uint16_t PROGMEM ne_combo[] = {KC_N, KC_E, COMBO_END};
-const uint16_t PROGMEM ei_combo[] = {KC_E, KC_I, COMBO_END};
-const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
-const uint16_t PROGMEM ni_combo[] = {KC_N, KC_I, COMBO_END};
-
-combo_t key_combos[] = {
-  [ST_CTL] = COMBO(st_combo, KC_LCTL),
-  [RS_SFT] = COMBO(rs_combo, KC_LSFT),
-  [AR_GUI] = COMBO(ar_combo, KC_LGUI),
-  [RT_ALT] = COMBO(rt_combo, KC_LALT),
-  [NE_CTL] = COMBO(ne_combo, KC_LCTL),
-  [EI_SFT] = COMBO(ei_combo, KC_LSFT),
-  [IO_GUI] = COMBO(io_combo, KC_LGUI),
-  [NI_ALT] = COMBO(ni_combo, KC_LALT),
-};
+//enum combos {
+//  ST_CTL,
+//  RS_SFT,
+//  AR_GUI,
+//  RT_ALT,
+//  NE_CTL,
+//  EI_SFT,
+//  IO_GUI,
+//  NI_ALT
+//};
+//
+//const uint16_t PROGMEM st_combo[] = {KC_S, KC_T, COMBO_END};
+//const uint16_t PROGMEM rs_combo[] = {KC_R, KC_S, COMBO_END};
+//const uint16_t PROGMEM ar_combo[] = {KC_A, KC_R, COMBO_END};
+//const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
+//const uint16_t PROGMEM ne_combo[] = {KC_N, KC_E, COMBO_END};
+//const uint16_t PROGMEM ei_combo[] = {KC_E, KC_I, COMBO_END};
+//const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
+//const uint16_t PROGMEM ni_combo[] = {KC_N, KC_I, COMBO_END};
+//
+//combo_t key_combos[] = {
+//  [ST_CTL] = COMBO(st_combo, KC_LCTL),
+//  [RS_SFT] = COMBO(rs_combo, KC_LSFT),
+//  [AR_GUI] = COMBO(ar_combo, KC_LGUI),
+//  [RT_ALT] = COMBO(rt_combo, KC_LALT),
+//  [NE_CTL] = COMBO(ne_combo, KC_LCTL),
+//  [EI_SFT] = COMBO(ei_combo, KC_LSFT),
+//  [IO_GUI] = COMBO(io_combo, KC_LGUI),
+//  [NI_ALT] = COMBO(ni_combo, KC_LALT),
+//};
 
 // Left-hand home row mods colemak
 #define GUI_A LGUI_T(KC_A)
@@ -47,13 +54,14 @@ combo_t key_combos[] = {
 #define CTRL_T LCTL_T(KC_T)
 
 // Right-hand home row mods colemak
-#define CTL_N RCTL_T(KC_N)
+#define CTRL_N RCTL_T(KC_N)
 #define SFT_E RSFT_T(KC_E)
 #define ALT_I LALT_T(KC_I)
 #define GUI_O LGUI_T(KC_O)
 
 enum layer {
   COLEMAK,
+  GAME,
   NUM,
   SYM,
   FN
@@ -72,7 +80,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      | ESC  |Num_SP|  Tab |FN_Ent|Sym_BS|  Del |      |      |Right |
  * `-----------------------------------------------------------------------------------'
  */
+
 [COLEMAK] = LAYOUT(
+  KC_LALT, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_Y,    KC_L,    KC_U,    KC_K,    SE_ARNG, SE_ODIA,
+  KC_LCTL, GUI_A,   ALT_R,   SFT_S,   CTRL_T,  KC_G,    KC_M,    CTRL_N,  SFT_E,   ALT_I,   GUI_O,   SE_ADIA,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_J,    KC_H,    KC_COMM, KC_DOT,  SE_MINS, KC_LGUI,
+                             KC_ESC,  NUM_SPC, CTL_TAB, FN_ENT,  SYM_BSP, ALT_DEL
+),
+
+[GAME] = LAYOUT(
   KC_LALT, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_Y,    KC_L,    KC_U,    KC_K,    SE_ARNG, SE_ODIA,
   KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    SE_ADIA,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_J,    KC_H,    KC_COMM, KC_DOT,  SE_MINS, KC_LGUI,
@@ -81,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* SYM
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   F  |   P  |   B  |   Y  |   L  |   U  |   K  |   Å  |  Ä   |
+ * | RST  |   §  |   '  |   /  |   ~  |      |   ^  |   |  |   \  |   "  |      |  Ä   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Esc  |   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   I  |   O  |  Ö   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -91,12 +107,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [SYM] = LAYOUT(
-  QK_BOOT, SE_SECT, SE_ACUT, SE_SLSH, SE_TILD, _______, SE_CIRC, SE_PIPE, SE_BSLS, SE_DIAE, SE_ODIA, _______,
-  _______, SE_EXLM, SE_DLR,  SE_LPRN, SE_LBRC, SFT_INS, _______, SE_RBRC, SE_RPRN, SE_HASH, SE_QUES, _______,
-  _______, SE_QUOT, SE_PERC, SE_LCBR, SE_LABK, _______, _______, SE_RABK, SE_RCBR, SE_AMPR, _______, _______,
+  QK_BOOT, SE_SECT, SE_ACUT, SE_SLSH, SE_TILD, _______, SE_CIRC, SE_PIPE, SE_BSLS, SE_DIAE, _______, TG(GAME),
+  larrow,  SE_EXLM, SE_DLR,  SE_LPRN, SE_LBRC, SFT_INS, _______, SE_RBRC, SE_RPRN, SE_HASH, SE_QUES, rarrow,
+  _______, SE_QUOT, SE_PERC, SE_LCBR, SE_LABK, KC_SPC,  _______, SE_RABK, SE_RCBR, SE_AMPR, others0, _______,
                              SE_AT,   SE_EQL,  SE_DQUO, _______, _______, _______
 ),
-
+ 
 /* NUM
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   F  |   P  |   B  |   Y  |   L  |   U  |   K  |   Å  |  Ä   |
@@ -110,8 +126,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [NUM] = LAYOUT(
   _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, SE_7,    SE_8,    SE_9,    SE_ADIA, _______,
-  _______, SE_PLUS, SE_MINS, SE_ASTR, SE_SLSH, SE_CIRC, _______, SE_4,    SE_5,    SE_6,    _______, _______,
-  _______, _______, _______, KC_VOLD, KC_VOLU, _______, KC_SPC,  SE_1,    SE_2,    SE_3,    _______, _______,
+  _______, SE_PLUS, SE_MINS, SE_ASTR, SE_SLSH, SE_CIRC, KC_SPC,  SE_4,    SE_5,    SE_6,    SE_QUOT, _______,
+  _______, _______, _______, KC_VOLD, KC_VOLU, _______, KC_BSPC, SE_1,    SE_2,    SE_3,    _______, _______,
                              _______, _______, _______, SE_COMM, SE_0,    SE_DOT
 ),
 
@@ -134,3 +150,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 };
 
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case rarrow :
+        if (record->event.pressed) {
+            // when keycode rarrow is pressed
+            SEND_STRING(" => ");
+        } else {
+            // when keycode rarrow is released
+        }
+        break;
+
+    case larrow :
+        if (record->event.pressed) {
+            // when keycode rarrow is pressed
+            SEND_STRING(" <= ");
+        } else {
+            // when keycode rarrow is released
+        }
+        break;
+
+    case others0 :
+        if (record->event.pressed) {
+            // when keycode rarrow is pressed
+            SEND_STRING(" <= (others => '0');");
+        } else {
+            // when keycode rarrow is released
+        }
+        break;
+    }
+    return true;
+};
